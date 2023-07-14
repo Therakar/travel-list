799485a4b2248442c738a25329cca29f258c1540
Therakar <matteo.pau200@gmail.com>
2023-07-14 13:38:26 +0200

    adds some content and style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
-function Logo() {}
-function Form() {}
-function PackingList() {}
-function Stats() {}
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip? 😍</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>🧳 You have X items on your list and you already packed X (X%)</em>
+    </footer>
+  );
+}
